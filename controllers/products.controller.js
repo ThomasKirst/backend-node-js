78edc1301ef00577680bc3f5f570835ad650d62e
Thomas Kirst <thomas@moneygarden.de>
2021-12-19 15:27:15 +0100

    Added file database and functionality to store products in it. Also added an index / maintenance route.

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,3 +1,6 @@
+import { v4 as uuidv4 } from 'uuid';
+import { readFile, writeFile } from 'fs';
+
 function getProductForm(request, response) {
   response.send(`
       <form action="/products" method="POST">
@@ -8,8 +11,26 @@ function getProductForm(request, response) {
 }
 
 function postProduct(request, response) {
-  const { productname } = request.body;
-  response.send(`<h1>Product ${productname} has been added.`);
+  const newProduct = { ...request.body, id: uuidv4() };
+
+  readFile('db.json', (error, fileContents) => {
+    if (error) {
+      console.error(error.message);
+      return response.json({ error: error.message });
+    }
+    const database = JSON.parse(fileContents);
+    const products = database.products;
+    products.push(newProduct);
+
+    writeFile('db.json', JSON.stringify(database, null, 2), (error) => {
+      if (error) {
+        console.error(error.message);
+        response.json({ error: error.message });
+        return;
+      }
+      response.json(newProduct);
+    });
+  });
 }
 
 export { getProductForm, postProduct };
