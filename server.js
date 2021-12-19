9b48e6870507d4bc51bc488df752675212f52517
Thomas Kirst <thomas@moneygarden.de>
2021-12-19 15:15:01 +0100

    Extracted routes into a folder and their respective files. Tidied up main server.js file.

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 import express from 'express';
-import {
-  getProductForm,
-  postProduct,
-} from './controllers/products.controller.js';
 
-import {
-  getCustomerForm,
-  postCustomer,
-} from './controllers/customers.controller.js';
+import productsRoutes from './routes/products.routes.js';
+import customersRoutes from './routes/customers.routes.js';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', getProductForm);
-app.post('/products', postProduct);
-
-app.get('/customers', getCustomerForm);
-app.post('/customers', postCustomer);
+app.use(productsRoutes);
+app.use(customersRoutes);
 
 app.listen(4000, () => console.log('Server is started.'));
