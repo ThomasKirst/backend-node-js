50e9e368115cdbec3f9d235473781838f2bb91d3
Thomas Kirst <thomas@moneygarden.de>
2021-12-19 15:07:38 +0100

    Added the customers controller for a form and response

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ import {
   postProduct,
 } from './controllers/products.controller.js';
 
+import {
+  getCustomerForm,
+  postCustomer,
+} from './controllers/customers.controller.js';
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', getProductForm);
 app.post('/products', postProduct);
 
-app.get('/', getProductForm);
+app.get('/customers', getCustomerForm);
+app.post('/customers', postCustomer);
 
 app.listen(4000, () => console.log('Server is started.'));
